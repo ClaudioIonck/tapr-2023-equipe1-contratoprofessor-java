3647f17ed30c9aff5a31c064d63f1a773e214c02
Claudio Luiz Defrein Ionck <78597253+ClaudioIonck@users.noreply.github.com>
2023-10-10 00:58:37 +0000

    Update

--- microservprofessor/src/main/java/br/edu/univille/microservprofessor/entity/Professor.java
+++ microservprofessor/src/main/java/br/edu/univille/microservprofessor/entity/Professor.java
@@ -1,5 +1,30 @@
 package br.edu.univille.microservprofessor.entity;
 
+import org.springframework.data.annotation.Id;
+
+import com.azure.spring.data.cosmos.core.mapping.Container;
+import com.azure.spring.data.cosmos.core.mapping.GeneratedValue;
+
+@Container(containerName = "Professor")
+
 public class Professor {
+    @Id
+    @GeneratedValue
+    public String id;
+    public String nome;
+    public String email;
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
     
 }
