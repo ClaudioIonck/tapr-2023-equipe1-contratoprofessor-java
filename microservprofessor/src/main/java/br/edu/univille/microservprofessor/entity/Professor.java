71f03db315f8e493c895ae17fe6b5ae17641ec13
Claudio Luiz Defrein Ionck <78597253+ClaudioIonck@users.noreply.github.com>
2023-10-10 01:03:01 +0000

    GettersEndSetters

--- microservprofessor/src/main/java/br/edu/univille/microservprofessor/entity/Professor.java
+++ microservprofessor/src/main/java/br/edu/univille/microservprofessor/entity/Professor.java
@@ -15,6 +15,12 @@ public class Professor {
     public String nome;
     public String email;
 
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
     public String getNome() {
         return nome;
     }
