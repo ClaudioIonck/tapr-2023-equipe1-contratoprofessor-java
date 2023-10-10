489927dc618fc66bb49b0bb333f15e7b5ea1e97f
Claudio Luiz Defrein Ionck <78597253+ClaudioIonck@users.noreply.github.com>
2023-10-10 11:40:35 +0000

    update

--- microservprofessor/src/main/java/br/edu/univille/microservprofessor/entity/Professor.java
+++ microservprofessor/src/main/java/br/edu/univille/microservprofessor/entity/Professor.java
@@ -4,7 +4,6 @@ import org.springframework.data.annotation.Id;
 
 import com.azure.spring.data.cosmos.core.mapping.Container;
 import com.azure.spring.data.cosmos.core.mapping.GeneratedValue;
-import com.azure.spring.data.cosmos.core.mapping.PartitionKey;
 
 @Container(containerName = "Professor")
 
