f51a043f5d9e15f6584c00aa7e15f57984b2c884
Claudio Luiz Defrein Ionck <78597253+ClaudioIonck@users.noreply.github.com>
2023-10-24 00:29:14 +0000

    ContratoAPI

--- microservprofessor/src/main/java/br/edu/univille/microservcontratoprofessor/controller/ContratoAPIController.java
+++ microservprofessor/src/main/java/br/edu/univille/microservcontratoprofessor/controller/ContratoAPIController.java
@@ -8,11 +8,13 @@ import org.springframework.web.bind.annotation.RestController;
 import br.edu.univille.microservcontratoprofessor.entity.Contrato;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 @RequestMapping("/api/v1/contratos")
 public class ContratoAPIController {
+    @GetMapping
     public ResponseEntity<List<Contrato>> listarContratos() {
         var listaContratos = new ArrayList<Contrato>();
       
